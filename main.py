961c1a8e45e94388058bc0399146cafffb304608
MarijaCausevska <43523151+MarijaCausevska@users.noreply.github.com>
2021-04-22 21:42:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -102,71 +102,71 @@ def Evaluate(model=None):
     torch.cuda.empty_cache()
     return
 
-def Predict():
-    reverse_task_type = args.task_type.split('-')[1] + '-' + args.task_type.split('-')[0]
-    def Filter(x):
-        if x['init_pred'] in conf.relation_type[args.task_type]:
-            if x['reverse_pred'] not in conf.relation_type[reverse_task_type]:
+#def Predict():
+ #   reverse_task_type = args.task_type.split('-')[1] + '-' + args.task_type.split('-')[0]
+ #   def Filter(x):
+ #       if x['init_pred'] in conf.relation_type[args.task_type]:
+ #           if x['reverse_pred'] not in conf.relation_type[reverse_task_type]:
                 # init_pred is a correct relation but reverse_pred not
-                return 'init_pred'
-            else:
+ #               return 'init_pred'
+ #           else:
                 # init_pred and reverse_pred both are correct relations
-                if x['init_pred_prob'] >= x['reverse_pred_prob']:
-                    # init_pred_prob greater than or equal to reverse_pred_prob
-                    return 'init_pred'
-                else:
-                    return 'reverse_pred'
-        else:
-            if x['reverse_pred'] not in conf.relation_type[reverse_task_type]:
-                # init_pred and reverse_pred both are uncorrect relations
-                return 'uncorrect'
-            else:
-                # reverse_pred is a correct relation but init_pred not
-                return 'reverse_pred'
-    marked_sentence_df = pd.read_csv('./data/marked_sentence.csv')
-    label_df = pd.read_csv('./data/%s_label.csv'%args.task_type)
-    tokenizer,model = Bert_model(args.task_type,'./model/%s'%args.task_type)
-    model = model.to(device)
-    preds = []
-    preds_prob = []
-    reverse_preds = []
-    reverse_preds_prob = []
-    for data in tqdm(predict_data_loader):
-        ids,reverse_ids = [t.to(device) for t in data]
-        outputs = model(input_ids=ids)
-        logits = outputs[0]
-        pred_prob, pred = torch.max(F.softmax(logits.data,1), 1)
-        preds.extend(list(pred.cpu().detach().numpy()))
-        preds_prob.extend(list(pred_prob.cpu().detach().numpy()))
-        reverse_outputs = model(input_ids=reverse_ids)
-        reverse_logits = reverse_outputs[0]
-        reverse_pred_prob, reverse_pred = torch.max(F.softmax(reverse_logits.data,1), 1)
-        reverse_preds.extend(list(reverse_pred.cpu().detach().numpy()))
-        reverse_preds_prob.extend(list(reverse_pred_prob.cpu().detach().numpy()))
+ #               if x['init_pred_prob'] >= x['reverse_pred_prob']:
+ #                   # init_pred_prob greater than or equal to reverse_pred_prob
+ #                   return 'init_pred'
+ #               else:
+ #                   return 'reverse_pred'
+ #       else:
+ #           if x['reverse_pred'] not in conf.relation_type[reverse_task_type]:
+ #               # init_pred and reverse_pred both are uncorrect relations
+ #               return 'uncorrect'
+ #           else:
+ #               # reverse_pred is a correct relation but init_pred not
+ #               return 'reverse_pred'
+  #  marked_sentence_df = pd.read_csv('./data/marked_sentence.csv')
+  #  label_df = pd.read_csv('./data/%s_label.csv'%args.task_type)
+  #  tokenizer,model = Bert_model(args.task_type,'./model/%s'%args.task_type)
+   # model = model.to(device)
+    #preds = []
+    #preds_prob = []
+    #reverse_preds = []
+    #reverse_preds_prob = []
+    #for data in tqdm(predict_data_loader):
+    #    ids,reverse_ids = [t.to(device) for t in data]
+    #    outputs = model(input_ids=ids)
+    #    logits = outputs[0]
+    #    pred_prob, pred = torch.max(F.softmax(logits.data,1), 1)
+    #    preds.extend(list(pred.cpu().detach().numpy()))
+    #    preds_prob.extend(list(pred_prob.cpu().detach().numpy()))
+     #   reverse_outputs = model(input_ids=reverse_ids)
+      #  reverse_logits = reverse_outputs[0]
+      #  reverse_pred_prob, reverse_pred = torch.max(F.softmax(reverse_logits.data,1), 1)
+       # reverse_preds.extend(list(reverse_pred.cpu().detach().numpy()))
+       # reverse_preds_prob.extend(list(reverse_pred_prob.cpu().detach().numpy()))
 
-    pred_df = pd.DataFrame({'marked_sentence':marked_sentences,'init_pred':preds,'init_pred_prob':preds_prob,'reverse_pred':reverse_preds,'reverse_pred_prob':reverse_preds_prob})
-    # map label(0, 1, 2...) to raw label(T, C, Sa...)
-    pred_df['init_pred'] = pred_df['init_pred'].replace(dict(label_df.set_index(['label'])['label_raw']))
-    pred_df['reverse_pred'] = pred_df['reverse_pred'].replace(dict(label_df.set_index(['label'])['label_raw']))
-    # judge the order of a pair of entities
-    pred_df['filter'] = pred_df.apply(lambda x:Filter(x), axis=1)
-    pred_df['pred'] = pred_df['init_pred']
-    pred_df['pred_prob'] = pred_df['init_pred_prob']
-    pred_df.loc[pred_df['filter']=='reverse_pred','pred'] = pred_df.loc[pred_df['filter']=='reverse_pred','reverse_pred']
-    pred_df.loc[pred_df['filter']=='reverse_pred','pred_prob'] = pred_df.loc[pred_df['filter']=='reverse_pred','reverse_pred_prob']
-    pred_df = pred_df.loc[pred_df['filter']!='uncorrect']
-    pred_df = marked_sentence_df.merge(pred_df,how='inner',on='marked_sentence')
-    pred_df['init_start_entity'] = pred_df['start_entity']
-    pred_df['init_start_entity_type'] = pred_df['start_entity_type']
-    pred_df.loc[pred_df['filter']=='reverse_pred','start_entity'] = pred_df.loc[pred_df['filter']=='reverse_pred','end_entity']
-    pred_df.loc[pred_df['filter']=='reverse_pred','start_entity_type'] = pred_df.loc[pred_df['filter']=='reverse_pred','end_entity_type']
-    pred_df.loc[pred_df['filter']=='reverse_pred','end_entity'] = pred_df.loc[pred_df['filter']=='reverse_pred','init_start_entity']
-    pred_df.loc[pred_df['filter']=='reverse_pred','end_entity_type'] = pred_df.loc[pred_df['filter']=='reverse_pred','init_start_entity_type']
-    pred_df.drop(['init_start_entity','init_start_entity_type'],axis=1,inplace=True)
-    pred_df.to_csv('./data/%s/%s_pred.csv'%(args.prediction_path,args.task_type),index=False)
-    torch.cuda.empty_cache()
-    return
+  #  pred_df = pd.DataFrame({'marked_sentence':marked_sentences,'init_pred':preds,'init_pred_prob':preds_prob,'reverse_pred':reverse_preds,'reverse_pred_prob':reverse_preds_prob})
+  #  # map label(0, 1, 2...) to raw label(T, C, Sa...)
+  #  pred_df['init_pred'] = pred_df['init_pred'].replace(dict(label_df.set_index(['label'])['label_raw']))
+  #  pred_df['reverse_pred'] = pred_df['reverse_pred'].replace(dict(label_df.set_index(['label'])['label_raw']))
+  #  # judge the order of a pair of entities
+  #  pred_df['filter'] = pred_df.apply(lambda x:Filter(x), axis=1)
+  #  pred_df['pred'] = pred_df['init_pred']
+  #  pred_df['pred_prob'] = pred_df['init_pred_prob']
+  #  pred_df.loc[pred_df['filter']=='reverse_pred','pred'] = pred_df.loc[pred_df['filter']=='reverse_pred','reverse_pred']
+ #   pred_df.loc[pred_df['filter']=='reverse_pred','pred_prob'] = pred_df.loc[pred_df['filter']=='reverse_pred','reverse_pred_prob']
+ #   pred_df = pred_df.loc[pred_df['filter']!='uncorrect']
+ #   pred_df = marked_sentence_df.merge(pred_df,how='inner',on='marked_sentence')
+  #  pred_df['init_start_entity'] = pred_df['start_entity']
+  #  pred_df['init_start_entity_type'] = pred_df['start_entity_type']
+  #  pred_df.loc[pred_df['filter']=='reverse_pred','start_entity'] = pred_df.loc[pred_df['filter']=='reverse_pred','end_entity']
+  #  pred_df.loc[pred_df['filter']=='reverse_pred','start_entity_type'] = pred_df.loc[pred_df['filter']=='reverse_pred','end_entity_type']
+  #  pred_df.loc[pred_df['filter']=='reverse_pred','end_entity'] = pred_df.loc[pred_df['filter']=='reverse_pred','init_start_entity']
+  #  pred_df.loc[pred_df['filter']=='reverse_pred','end_entity_type'] = pred_df.loc[pred_df['filter']=='reverse_pred','init_start_entity_type']
+  #  pred_df.drop(['init_start_entity','init_start_entity_type'],axis=1,inplace=True)
+  #  pred_df.to_csv('./data/%s/%s_pred.csv'%(args.prediction_path,args.task_type),index=False)
+  # torch.cuda.empty_cache()
+  #  return
 
 Train(evalEpochs=5)
 Evaluate()
-Predict()
+#Predict()
