ca01a9845c373205a718434586907f923cd33975
MarijaCausevska <43523151+MarijaCausevska@users.noreply.github.com>
2021-04-22 21:32:05 +0200

    Update data.py

--- data.py
+++ data.py
@@ -71,7 +71,7 @@ class Prepare_Data(Conf):
         return train_data_loader,test_data_loader
 
     def Prepare_predict_data(self,tokenizer,bs):
-        marked_sentence_df = pd.read_csv('./data/marked_sentence.csv')
+        marked_sentence_df = pd.read_csv('./data/marked_sentence.csv',header=None,error_bad_lines=False)
         marked_sentences = marked_sentence_df.loc[(marked_sentence_df['start_entity_type'].apply(lambda x:x.lower())==self.task_type.split('-')[0])&\
                                                   (marked_sentence_df['end_entity_type'].apply(lambda x:x.lower())==self.task_type.split('-')[1]),'marked_sentence']
 
