b257e963afa0b3eadeb3917024d0f1f2d0486e5f
MarijaCausevska <43523151+MarijaCausevska@users.noreply.github.com>
2021-05-11 12:45:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ conf = Conf()
 def Parse_args():
     args = argparse.ArgumentParser()
     args.add_argument('--task_type',
-                      default='chemical-disease', help='task type:chemical-disease,chemical-gene,gene-disease')
+                      default='chemical-gene', help='task type:chemical-disease,chemical-gene,gene-disease')
     args.add_argument('--confidence_limit', type=float,
                       default=-1.0, help='dependency path lower confidence limit, use suggestion value if it equal -1.0. \
                       suggestion value:0.9 for chemical-disease; 0.5 for chemical-gene; 0.6 for gene-disease; 0.9 for gene-gene')
@@ -216,6 +216,6 @@ def Evaluate(model=None):
   # torch.cuda.empty_cache()
   #  return
 
-Train(evalEpochs=5)
+#Train(evalEpochs=5)
 Evaluate()
 #Predict()
