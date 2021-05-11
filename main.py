7c6794774a550b1030015d1ab09bd4d1abb60fc1
MarijaCausevska <43523151+MarijaCausevska@users.noreply.github.com>
2021-05-11 19:34:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ conf = Conf()
 def Parse_args():
     args = argparse.ArgumentParser()
     args.add_argument('--task_type',
-                      default='gene-disease', help='task type:chemical-disease,chemical-gene,gene-disease')
+                      default='gene-gene', help='task type:chemical-disease,chemical-gene,gene-disease')
     args.add_argument('--confidence_limit', type=float,
                       default=-1.0, help='dependency path lower confidence limit, use suggestion value if it equal -1.0. \
                       suggestion value:0.9 for chemical-disease; 0.5 for chemical-gene; 0.6 for gene-disease; 0.9 for gene-gene')
@@ -109,7 +109,7 @@ def Train(evalEpochs=None):
 
 def Evaluate(model=None):
     if model == None:
-        tokenizer,model = Bert_model(args.task_type,'./models/%s'%args.task_type)#'bert-base-cased')
+        tokenizer,model = Bert_model(args.task_type,'./models/%s/'%args.task_type)#'bert-base-cased')
         model = model.to(device)
     test_preds,test_labels = [],[]
     #total loss for this epoch
